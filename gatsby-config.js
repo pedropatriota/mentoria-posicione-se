103fe5f9e1392e0540fbbb26cdbd2f550e0aa942
pedropatriota <pedro.p.patriota@gmail.com>
2021-12-21 08:54:40 -0300

    adding dot env for analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Clínica Allure`,
@@ -89,52 +93,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: 'UA-215638171-1',
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        // exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        // cookieDomain: "example.com",
-        // defaults to false
-        enableWebVitalsTracking: true,
+        trackingId: process.env.GA_TRACKING_ID,
       },
     },
     {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
         id: 'GTM-KPVLJ4B',
-
         includeInDevelopment: false,
-
         defaultDataLayer: { platform: 'gatsby' },
-
-        // gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING',
-        // gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME',
-        // dataLayerName: 'YOUR_DATA_LAYER_NAME',
-
         routeChangeEventName: 'Allure_Event',
-        // Defaults to false
-        enableWebVitalsTracking: true,
-        // Defaults to https://www.googletagmanager.com
-        // selfHostedOrigin: 'YOUR_SELF_HOSTED_ORIGIN',
       },
     },
     {
