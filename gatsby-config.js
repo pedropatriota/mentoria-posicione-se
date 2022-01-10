619d0a1347ae30032bf3f6b16530d7647b41799c
pedropatriota <pedro.p.patriota@gmail.com>
2022-01-10 07:03:06 -0300

    fix env

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,7 +99,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GA_TRACKING_ID,
+        trackingId: 'UA-215638171-1',
       },
     },
     {
